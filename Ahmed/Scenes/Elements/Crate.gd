1f18bc875693485e757b4db58904c33082aa46e2
Ahmed Elgoni <ai.elgoni@gmail.com>
2022-05-22 19:53:04 +0200

    You can now drop items

--- Ahmed/Scenes/Elements/Crate.gd
+++ Ahmed/Scenes/Elements/Crate.gd
@@ -1,6 +1,16 @@
 extends KinematicBody2D
 
-var itemName : String = "Crate"
+export var gravity : int = 1600
+
+export var itemName : String = "Crate"
+export var isActive : bool = false
+
+var _velocity : Vector2 = Vector2()
+
+func _physics_process (delta):
+	if isActive:
+		_velocity.y += gravity * delta
+		_velocity = move_and_slide(_velocity, Vector2.UP)
 
 func destroy():
 	queue_free()
