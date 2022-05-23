b86f48c9593f7963a54f64540477fd31c61287ff
Ahmed Elgoni <ai.elgoni@gmail.com>
2022-05-23 18:46:58 +0200

    Fixed bug with crate name not going away when picked up

--- Ahmed/Scenes/Elements/Crate.gd
+++ Ahmed/Scenes/Elements/Crate.gd
@@ -6,6 +6,7 @@ export var itemName : String = "Crate"
 export var isActive : bool = false
 
 onready var collisionShape : Area2D = $Area2D
+onready var itemNameNode : Node2D = $ItemName
 var _velocity : Vector2 = Vector2()
 
 func _physics_process (delta):
@@ -13,12 +14,16 @@ func _physics_process (delta):
 		_velocity.y += gravity * delta
 		_velocity = move_and_slide(_velocity, Vector2.UP)
 		
+		itemNameNode.visible = true
+		
 		var collidingBodies : Array = collisionShape.get_overlapping_bodies()
 		for body in collidingBodies:
 			if body.name == "Player":
 				if Input.is_action_just_pressed("interact"):
 					isActive = false
 					get_tree().call_group("Inventory", "_onItemPickedUp", self)
+	else:
+		itemNameNode.visible = false
 
 func destroy():
 	queue_free()
